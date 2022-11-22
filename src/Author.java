830b814b88fafbe54993c3d0746f82e67c438160
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-22 11:55:29 -0600

    fixed errors without @suppresswarnings

--- src/Author.java
+++ src/Author.java
@@ -4,6 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings({"unchecked"})
 public class Author extends Person {
     private List books;
 
@@ -27,8 +28,7 @@ public class Author extends Person {
     public void addBook(String book) {
         books.add(book);
     }
-
-    @Override
+//    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
